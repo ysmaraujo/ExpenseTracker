42478c7804740a40e24823c0cffb070fc672a9ee
Yasmin Araujo <yasminnaraujo@hotmail.com>
2024-04-27 17:19:47 +0100

    IntroScreen TabView Tab Bar Items

---
+++ ExpenseTracker/Helpers/Constants.swift
@@ -0,0 +1,18 @@
+//
+//  Constants.swift
+//  ExpenseTracker
+//
+//  Created by Yasmin araujo on 27/04/2024.
+//
+
+import SwiftUI
+
+struct Constants: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Constants()
+}
---
+++ ExpenseTracker/Model/IntroScreen.swift
@@ -0,0 +1,18 @@
+//
+//  IntroScreen.swift
+//  ExpenseTracker
+//
+//  Created by Yasmin araujo on 27/04/2024.
+//
+
+import SwiftUI
+
+struct IntroScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    IntroScreen()
+}
